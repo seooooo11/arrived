4c6a07199041f4ca09d5aa33397cd5bc225725f6
장환상 <janhan97@naver.com>
2023-09-15 18:46:38 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ input.onGesture(Gesture.Shake, function () {
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(0)
     basic.showNumber(123)
+    basic.showNumber(456)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(최종도착시간)
